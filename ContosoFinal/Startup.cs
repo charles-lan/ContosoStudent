280cec9618e58beb28b26f5007b69424f1d7ce0f
Charles Lan <charlis.lan@gmail.com>
2015-11-25 14:00:57 +1300

    Created project and added Telemetry.

---
+++ ContosoFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContosoFinal.Startup))]
+namespace ContosoFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
